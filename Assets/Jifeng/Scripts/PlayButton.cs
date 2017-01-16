163784c8834a4509c66ebe368f4110c62ff2fd43
cc-setwo <cc.setwo@gmail.com>
2017-01-16 18:32:43 +0200

    no message

---
+++ Assets/Jifeng/Scripts/PlayButton.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Jifeng.DemoSoftVr
+{
+    // a simple on event listener.it receives slide button submit event.
+    public class PlayButton : MonoBehaviour
+    {
+        public static string Room = "1";
+
+        public void OnButtonConfirmed()
+        {
+            switch (gameObject.transform.parent.gameObject.name.ToLower())
+            {
+                case "front":
+                {
+                    if (Room == "2")
+                    {
+                        GameObject.FindGameObjectWithTag("Player").transform.position =
+                            new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x + 50,
+                                GameObject.FindGameObjectWithTag("Player").transform.position.y,
+                                GameObject.FindGameObjectWithTag("Player").transform.position.z);
+                        Room = "1";
+                    }
+                        if (Room == "3")
+                        {
+                            GameObject.FindGameObjectWithTag("Player").transform.position =
+                                new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x - 50,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.y,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.z);
+                            Room = "1";
+                        }
+                    }
+                    break;
+                case "right":
+                {
+                        if (Room == "1")
+                        {
+                            GameObject.FindGameObjectWithTag("Player").transform.position =
+                                new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x + 50,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.y,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.z);
+                            Room = "2";
+                        }
+                        if (Room == "3")
+                        {
+                            GameObject.FindGameObjectWithTag("Player").transform.position =
+                                new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x + 100,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.y,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.z);
+                            Room = "2";
+                        }
+                    }
+                    break;
+                case "left":
+                {
+                        if (Room == "1")
+                        {
+                            GameObject.FindGameObjectWithTag("Player").transform.position =
+                                new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x - 50,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.y,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.z);
+                            Room = "3";
+                        }
+                        if (Room == "2")
+                        {
+                            GameObject.FindGameObjectWithTag("Player").transform.position =
+                                new Vector3(GameObject.FindGameObjectWithTag("Player").transform.position.x + 100,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.y,
+                                    GameObject.FindGameObjectWithTag("Player").transform.position.z);
+                            Room = "3";
+                        }
+                    }
+                    break;
+                case "back":
+                {
+                    //Application.LoadLevel("JfExperienceDemo");
+                }
+                    break;
+            }
+        }
+    }
+}
